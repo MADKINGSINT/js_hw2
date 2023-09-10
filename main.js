f20c40921d3e3505455bc002913ae19a018d96ed
madkings <unconfigured@null.spigotmc.org>
2023-09-10 17:11:03 +0300

    hw2 prepared

---
+++ main.js
@@ -0,0 +1,42 @@
+let num1  =  prompt();
+let num2  =  prompt();
+if (num1 <= 1) {
+    alert('true');
+} else {
+    alert('false');
+}
+if (num2 >= 3){
+    alert('true');
+} else {
+    alert('false');
+}
+
+
+
+
+
+let test = true;
+if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}
+(test === true) ? console.log('+++') : console.log('---');
+
+
+
+
+
+
+let day = prompt();
+if (day <= 11) {
+    alert('первая треть');
+
+}
+if (day<=21) {
+    alert('вторая треть');
+
+}
+else {
+    alert('третья треть');
+}
